dce2c8864f79a96d1c7a27362f40514d7d49e886
Lakshaymiddha <Lakshaymiddha@gmail.com>
2022-02-13 14:20:27 +0530

    Initial Version

---
+++ assets/js/script.js
@@ -0,0 +1,83 @@
+$( document ).ready(function() {
+    var w = window.innerWidth;
+
+    if(w > 767){
+        $('#menu-jk').scrollToFixed();
+    }else{
+       // $('#menu-jk').scrollToFixed();
+    }
+
+
+
+})
+
+
+$( document ).ready(function() {
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 5000,
+        // outerHeight:100,
+        // autoHeight:true,
+        nav:false,
+        // navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+
+});
+
+
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+
+// Manually Added to disable (Save image as)
+
+
+// $(document).ready(function(){
+//     $('img').mousedown(function (e) {
+//     if(e.button == 2) { // right click
+//       return false; // do nothing!
+//     }
+//   }
+// }
